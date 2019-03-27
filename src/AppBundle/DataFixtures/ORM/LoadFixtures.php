fbf293c850b454c156ce5d6529ecdd787d151d0e
Chris Goode <hello@chrisgoode.me>
2019-03-27 00:29:50 -0400

    Updated fixtures.

--- src/AppBundle/DataFixtures/ORM/LoadFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -18,6 +18,19 @@ class LoadFixtures implements FixtureInterface
     public function product()
     {
 
-        return 'product_'.random_int(0,999999999);
+        return 'product_'.random_int(0,999999);
+    }
+
+    public function provider()
+    {
+        $providers = [
+            'Provider_1',
+            'Provider_2',
+            'Provider_3',
+            'Provider_4',
+            'Provider_5',
+        ];
+        $key = array_rand($providers);
+        return $providers[$key];
     }
 }
