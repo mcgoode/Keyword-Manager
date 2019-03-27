e17a63c583856eb3ebf3ac439c712dcb21980cfd
Chris Goode <hello@chrisgoode.me>
2019-03-27 00:00:56 -0400

    One basic report is done

--- src/AppBundle/Controller/ReportController.php
+++ src/AppBundle/Controller/ReportController.php
@@ -2,20 +2,34 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Campaign;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class ReportController extends Controller
 {
+
     /**
-     * @Route("report/client/{id}/finished-campaigns")
+     * @Route("reports/list", name="report_list")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function getFinishedCampaignsForClientAction($id)
+    public function reportListAction()
     {
-        return $this->render('Report/get_finished_campaigns_for_client.html.twig', array(
-            // ...
-        ));
+        return $this->render('Report/list.html.twig');
+
+    }
+
+    /**
+     * @Route("reports/active-ended-campaigns",name="report_active_ended")
+     */
+    public function showActiveButEndedCampaignsAction()
+    {
+        $campaings = $this->getDoctrine()->getRepository(Campaign::class)->findByActiveAndPastEndDate();
+
+        return $this->render('Report/activeEndedCampaigns.html.twig', [
+            'campaings'=>$campaings
+        ]);
     }
 
     /**
